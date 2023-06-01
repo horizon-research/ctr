8200fa60b6072bba42ff407d33c18ce400ba9a42
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2023-06-01 14:00:27 -0400

    get_confusion_lines_xy uses linear_srgb. it's just fine.

--- dis/utils/color.js
+++ dis/utils/color.js
@@ -415,11 +415,11 @@ class discTestState {
 
     for (var t of [0, 1, 2]) {
       for (var p of [[1, 0, 0], [0, 1, 0], [0, 0, 1]]) {
-        // TODO: srgb is fine, since the lines in xy won't change
+        // using srgb is just fine, since the lines in xy won't change
         var line_RGB = this.confusion_lines_rgb[t];
-        var p0_RGB = new colorObj(math.add(p, math.multiply(line_RGB, 0.2)), 'v_rgb');
+        var p0_RGB = new colorObj(math.add(p, math.multiply(line_RGB, 0.2)), 'linear_srgb');
         var p0_xy = p0_RGB.xy;
-        var p1_RGB = new colorObj(p, 'v_rgb');
+        var p1_RGB = new colorObj(p, 'linear_srgb');
         var p1_xy = p1_RGB.xy;
         lines.push(normalize(math.subtract(p1_xy, p0_xy)));
       }
@@ -440,7 +440,6 @@ class discTestState {
 	// TODO: the idea is to make sure in each step at least one channel changes
 	// by setting the step size based on the first reversal color, but the
 	// implementation using deltaLUT is a hack and for now works only for sRGB
-    // TODO: there are a few uses of this line. should be part of the test object
     var line_RGB = this.confusion_lines_rgb[1]; // D line in RGB
 
     var deltaLUT = (page.bitdepth == 10) ? deltaLUT_10b : deltaLUT_8b;
