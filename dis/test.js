6995ce9b6897e03802274e4372c30832451dea4b
Yuhao Zhu <yzhu@utexas.edu>
2023-06-02 09:18:57 -0400

    move config display to result panel in test.

--- dis/test.js
+++ dis/test.js
@@ -14,7 +14,7 @@ state = new discTestState();
 var simMode = [false, 'no']; // enabled, choice
 var blindnessType = [false, 'pickd'];
 var simMethod = [false, 'm2'];
-var showXy = false, showRGB = false, showLab = false, showExp = true;
-page.configPage(simMode, blindnessType, simMethod, showXy, showRGB, showLab, showExp);
+var showXy = false, showRGB = false, showLab = false, showExp = true, showConfig = true;
+page.configPage(simMode, blindnessType, simMethod, showXy, showRGB, showLab, showExp, showConfig);
 
 page.submit(new colorObj([0.5, 0.9, 0.25], 'v_rgb'));
