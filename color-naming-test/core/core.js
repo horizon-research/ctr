539f1e3c6c98e72f48427a159f95d0c9724f8eb4
Yuhao Zhu <yuhao.ryan.zhu@gmail.com>
2023-08-02 13:19:29 -0400

    simple color naming test. done matching and training.

---
+++ color-naming-test/core/core.js
@@ -0,0 +1,58 @@
+function hex_to_srgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  var color = [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ];
+
+  return color;
+}
+
+function prepare_training() {
+  all_tests.base1 = $('#base1').val();
+  all_tests.match1 = $('#match1').val();
+  all_tests.base2 = $('#base2').val();
+  all_tests.match2 = $('#match2').val();
+  all_tests.base3 = $('#base3').val();
+  all_tests.match3 = $('#match3').val();
+
+  page.disColors[0] = '#color1';
+  page.disColors[1] = '#color2';
+  page.disColors[2] = '#color3';
+  page.disColors[3] = '#color4';
+  page.disColors[4] = '#color5';
+  page.disColors[5] = '#color6';
+
+  $('#color1').css('background-color', all_tests.base1);
+  $('#color2').css('background-color', all_tests.match1);
+  $('#color3').css('background-color', all_tests.base2);
+  $('#color4').css('background-color', all_tests.match2);
+  $('#color5').css('background-color', all_tests.base3);
+  $('#color6').css('background-color', all_tests.match3);
+
+  state.colors[0] = new colorObj(hex_to_srgb(all_tests.base1), 'srgb');
+  state.colors[1] = new colorObj(hex_to_srgb(all_tests.match1), 'srgb');
+  state.colors[2] = new colorObj(hex_to_srgb(all_tests.base2), 'srgb');
+  state.colors[3] = new colorObj(hex_to_srgb(all_tests.match2), 'srgb');
+  state.colors[4] = new colorObj(hex_to_srgb(all_tests.base3), 'srgb');
+  state.colors[5] = new colorObj(hex_to_srgb(all_tests.match3), 'srgb');
+
+  $('#color1').text(state.colors[0].srgb_name);
+  $('#color2').text(state.colors[1].srgb_name);
+  $('#color3').text(state.colors[2].srgb_name);
+  $('#color4').text(state.colors[3].srgb_name);
+  $('#color5').text(state.colors[4].srgb_name);
+  $('#color6').text(state.colors[5].srgb_name);
+
+  $(page.slider).on('input', function() {
+    $('.rot-label').html('Rotation Angle (Degree): ' + (this.value/Math.PI*180).toFixed(2) + '&#176;')
+    updatePlot(this.value, 0)
+  });
+  $(page.slider).prop('disabled', false);
+
+  $('#train-tab').trigger('click');
+  $('#title').text('Training');
+  set_keyboard_cb(false, true, false, false);
+}
+
---
+++ color-naming-test/nametest.js
@@ -0,0 +1,37 @@
+var p_line, d_line, t_line;
+var indices, testId;
+var prof, all_test_stats, dashboardName;
+var pageId = 0;
+var all_tests = {};
+var page_stats;
+
+class Profiler {
+  constructor() {
+    // time used in each trial
+    this.start = 0;
+    this.time_elapsed = [];
+    // number of rotations in each trial
+    this.incs = 0;
+    this.num_incrs = [];
+  }
+}
+
+prof = new Profiler();
+
+page = new pageObj('srgb');
+page.slider = '#customRange';
+page.slider_reset = '#reset';
+page.s11 = '#color1'
+page.s12 = '#color2'
+page.s13 = '#color3'
+page.s14 = '#color4'
+page.type = 0;
+page.sim = false;
+page.simMethod = 0;
+
+state = new discTestState(new colorObj([0, 0, 0], 'srgb'), 0.1,
+    ()=>{}, ()=>{},
+    ()=>{}, ()=>{});
+
+set_keyboard_cb(true, false, false, false);
+
