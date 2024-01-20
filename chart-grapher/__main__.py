711f1fab3f278ac6697a860ddf1b046d6c837629
Myocardinal <development@myocardinal.com>
2024-01-19 23:54:09 -0500

    Changed cvd to be abbreviated

--- chart-grapher/__main__.py
+++ chart-grapher/__main__.py
@@ -76,7 +76,7 @@ def graph_participant(participant_id: str, color_space: str, collection_strategy
 
 
 @command_group.command()
-@click.argument('cvd', type=str)
+@click.argument('cvd', type=click.Choice(['p', 'd', 't']))
 @click.argument('color_space', type=click.Choice(['uv', 'xy'], case_sensitive=False))
 @click.argument('collection_strategy', type=click.Choice(['mean', 'join']))
 def graph_cvd(cvd: str, color_space: str, collection_strategy: str):
@@ -86,6 +86,12 @@ def graph_cvd(cvd: str, color_space: str, collection_strategy: str):
 
     uv = color_space == "uv"
     join = collection_strategy == "join"
+    cvd_lookup = {
+        "d": "Deuteranopia",
+        "p": "Protanopia",
+        "t": "Tritanopia"
+    }
+    cvd = cvd_lookup[cvd.lower()]
 
     # Scan through all test files
     cvd_matched_tests = list()
